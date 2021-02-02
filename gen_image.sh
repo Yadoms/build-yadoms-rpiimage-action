c4a1c897ccc161d3518cc5bceecd2123fbb3b115
jano42 <decoretjm@gmail.com>
2021-02-02 09:35:06 +0100

    Add very first action Just generate Raspbian Lite image

---
+++ gen_image.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd pi-gen
+
+# Example for building a lite system
+echo "IMG_NAME='Raspbian'" > config
+touch ./stage3/SKIP ./stage4/SKIP ./stage5/SKIP
+touch ./stage4/SKIP_IMAGES ./stage5/SKIP_IMAGES
+sudo ./build-docker.sh
