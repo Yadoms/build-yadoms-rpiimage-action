a07113418a84e0a7f3af24041e163ed2b45de4d3
jano42 <decoretjm@gmail.com>
2021-02-02 11:02:27 +0100

    * manage output folder creation if not exists

--- gen_image.sh
+++ gen_image.sh
@@ -9,6 +9,11 @@ touch ./stage4/SKIP_IMAGES ./stage5/SKIP_IMAGES
 sudo ./build-docker.sh
 
 if [[ ! $# -eq 0 ]] ; then
+	if [ ! -d "$1" ]; then
+	  echo "Creating folder $1"
+	  mkdir -p $1
+	fi
+	
 	echo "Move resultig image to $1"
     mv deploy/* $1
 fi
