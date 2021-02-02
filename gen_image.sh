9e377b99881428f84043682a0b1132675ec70f5d
jano42 <decoretjm@gmail.com>
2021-02-02 11:40:40 +0100

    Add more logs

--- gen_image.sh
+++ gen_image.sh
@@ -8,7 +8,7 @@ touch ./stage3/SKIP ./stage4/SKIP ./stage5/SKIP
 touch ./stage4/SKIP_IMAGES ./stage5/SKIP_IMAGES
 sudo ./build-docker.sh
 
-if [[ ! $# -eq 0 ]] ; then
+if [ ! $# -eq 0 ]; then
 	if [ ! -d "$1" ]; then
 	  echo "Creating folder $1"
 	  mkdir -p $1
@@ -16,4 +16,7 @@ if [[ ! $# -eq 0 ]] ; then
 	
 	echo "Move resultig image to $1"
     mv deploy/* $1
+else
+	echo "No output folder provided"
+	echo "Deploy folder is $(pwd)/deploy"
 fi
