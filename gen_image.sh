f4ddfc7a6b9ab7aded3f80b035056f478ae4812a
Jean-Michel DECORET <jano42@users.noreply.github.com>
2021-02-03 09:10:10 +0100

    Update gen_image.sh

--- gen_image.sh
+++ gen_image.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 #retreive version from currently built file
 pwd
 ls -al
-export yadomsVersion=`ls builds/package/Yadoms*.tar.gz | head -1 | grep -oP '(?<=-).*(?=-)'`
+export yadomsVersion=`ls package/Yadoms*.tar.gz | head -1 | grep -oP '(?<=-).*(?=-)'`
 echo "Found Yadoms : $yadomsVersion"
 
 cd pi-gen
@@ -19,8 +19,11 @@ sudo touch ./stage3/SKIP ./stage4/SKIP ./stage5/SKIP
 sudo touch ./stage4/SKIP_IMAGES ./stage5/SKIP_IMAGES
 
 # copy stage99
+if [ -d "$DIR/stage99" ]; then
+	sudo rm -Rf $DIR/stage99
+fi
 sudo cp -rp $DIR/stage99 .
-sudo cp ../builds/package/Yadoms-$yadomsVersion-RaspberryPI.tar.gz ./stage99/03-yadoms/yadoms.tar.gz
+sudo cp ../package/Yadoms-$yadomsVersion-RaspberryPI.tar.gz ./stage99/03-yadoms/yadoms.tar.gz
 sudo chmod 777 ./stage99/03-yadoms/yadoms.tar.gz
 
 # generate image
@@ -33,7 +36,7 @@ if [ ! $# -eq 0 ]; then
 	fi
 	
 	echo "Move resultig image to $1"
-    sudo mv deploy/* $1
+    	sudo mv deploy/* $1
 	sudo chmod -R 777 $1
 else
 	echo "No output folder provided"
