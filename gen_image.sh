8f43685cc807d12ad25f9b330916643b78e3a239
Jean-Michel DECORET <jano42@users.noreply.github.com>
2021-02-05 08:52:46 +0100

    Update gen_image.sh

--- gen_image.sh
+++ gen_image.sh
@@ -1,18 +1,54 @@
 #!/bin/sh
 
+#Retreive current folder (folder of current script, not working folder)
 DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 
+#manage command line arguements
+LANG=EN
+DEPLOY_TO_CUSTOM_DIR=NO
+YADOMS_BINARY_PATH=package
+
+for i in "$@"
+do
+case $i in
+    -lfr|--lang_french)
+    LANG=FR
+    shift
+    ;;
+    -len|--lang_english)
+    LANG=EN
+    shift
+    ;;
+    -d=*|--deploy=*)
+    DEPLOY_TO_CUSTOM_DIR=YES
+    DEPLOY_DIR="${i#*=}"
+    shift
+    ;;
+    -y=*|--yadoms-path=*)
+    YADOMS_BINARY_PATH="${i#*=}"
+    shift
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
 
 #retreive version from currently built file
-export yadomsVersion=`ls package/Yadoms*.tar.gz | head -1 | grep -oP '(?<=-).*(?=-)'`
+export yadomsVersion=`ls ${YADOMS_BINARY_PATH}/Yadoms*.tar.gz | head -1 | grep -oP '(?<=-).*(?=-)'`
 echo "Found Yadoms : $yadomsVersion"
 
 cd pi-gen
 
-# Generate FR image
-sudo cp $DIR/config_fr ./config
+if [ $LANG = "FR" ]; then
+	# Generate FR image
+	sudo cp $DIR/config_fr ./config
+else
+	# Generate EN image
+	sudo cp $DIR/config_en ./config
+fi
 
-# disable stage 3, 4 and 5
+# disable stage 3, 4 and 5 from pi-gen
 sudo rm -Rf stage3
 sudo rm -Rf stage4
 sudo rm -Rf stage5
@@ -20,7 +56,7 @@ sudo rm -Rf stage5
 #sudo touch ./stage4/SKIP_IMAGES ./stage5/SKIP_IMAGES
 
 #disable generation of raspbian-lite image (our custom stage will generate raspbian-lite-yadoms)
-sudo touch ./stage2/SKIP_IMAGES 
+#sudo touch ./stage2/SKIP_IMAGES 
 
 # copy stage99
 if [ -d "stage99" ]; then
@@ -39,43 +75,16 @@ fi
 # generate image
 sudo ./build-docker.sh
 
-if [ ! $# -eq 0 ]; then
-	if [ ! -d "$1" ]; then
-	  echo "Creating folder $1"
-	  mkdir -p $1
+if [ $DEPLOY_TO_CUSTOM_DIR = "YES" ]; then
+	if [ ! -d "${DEPLOY_DIR}" ]; then
+	  echo "Creating folder ${DEPLOY_DIR}"
+	  mkdir -p ${DEPLOY_DIR}
 	fi
 	
-	echo "Move resultig FR image to $1"
-    sudo mv deploy/* $1
-	sudo chmod -R 777 $1
+	echo "Move resultig image to ${DEPLOY_DIR}"
+    	sudo mv deploy/* ${DEPLOY_DIR}
+	sudo chmod -R 777 ${DEPLOY_DIR}
 else
 	echo "No output folder provided"
 	echo "Deploy folder is $(pwd)/deploy"
 fi
-
-
-
-# Generate EN image
-sudo cp $DIR/config_en ./config
-
-if [ -d "work" ]; then
-	sudo rm -Rf work
-fi
-
-# generate image
-sudo ./build-docker.sh
-
-if [ ! $# -eq 0 ]; then
-	if [ ! -d "$1" ]; then
-	  echo "Creating folder $1"
-	  mkdir -p $1
-	fi
-	
-	echo "Move resultig EN image to $1"
-    sudo mv deploy/* $1
-	sudo chmod -R 777 $1
-else
-	echo "No output folder provided"
-	echo "Deploy folder is $(pwd)/deploy"
-fi
-
